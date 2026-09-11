8fa94f4b0ae9db0fde9d14291d7febe9c1cb8b4d
SAIFULLAH-AWAN <saifullahawan939@gmail.com>
2026-09-10 23:08:47 -0700

    Update test_all_items_to_cart.py

--- test_all_items_to_cart.py
+++ test_all_items_to_cart.py
@@ -23,24 +23,23 @@ def test_all_items_to_cart():
     try:
         driver.get("https://www.saucedemo.com")
 
-        # Login
         wait.until(EC.visibility_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
         driver.find_element(By.ID, "password").send_keys("secret_sauce")
         driver.find_element(By.ID, "login-button").click()
         time.sleep(7)
 
-        # Add all items to cart - USE JS CLICK
+        
         items = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "btn_inventory")))
         for item in items:
-            driver.execute_script("arguments[0].click();", item) # <-- CHANGED THIS
+            driver.execute_script("arguments[0].click();", item) 
             time.sleep(0.6)
 
-        # Go to cart - USE JS CLICK
+        
         cart_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link")))
-        driver.execute_script("arguments[0].click();", cart_button) # <-- CHANGED THIS
+        driver.execute_script("arguments[0].click();", cart_button) 
         time.sleep(1)
 
-        # Verify
+        
         cart_items = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "cart_item")))
         assert len(cart_items) == 6
         print("SUCCESS: All 6 items in cart")
