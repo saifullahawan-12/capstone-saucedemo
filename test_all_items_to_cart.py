3cf125f0d71e788629372030802fa85313b84c28
SAIFULLAH-AWAN <saifullahawan939@gmail.com>
2026-09-13 23:52:42 -0700

    Update test_all_items_to_cart.py

--- test_all_items_to_cart.py
+++ test_all_items_to_cart.py
@@ -26,7 +26,7 @@ def test_all_items_to_cart():
         wait.until(EC.visibility_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
         driver.find_element(By.ID, "password").send_keys("secret_sauce")
         driver.find_element(By.ID, "login-button").click()
-        time.sleep(9)
+        time.sleep(11)
 
         
         items = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "btn_inventory")))
