61c874a2b30bccde9d57a3097acbdae290a3d3ac
SAIFULLAH-AWAN <saifullahawan939@gmail.com>
2026-09-20 20:35:27 -0700

    Update test_all_items_to_cart.py

--- test_all_items_to_cart.py
+++ test_all_items_to_cart.py
@@ -1,54 +1,45 @@
 import os
 import time
+import tempfile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-if os.getenv("CI"):
+def get_chrome_options():
+    options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-allow-origins=*")
-    options.add_argument("--user-data-dir=C:\\Windows\\Temp\\chrome-user-data")
+    # FIX: unique temp folder for each worker in parallel
+    options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
+    return options
 
 def test_all_items_to_cart():
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=get_chrome_options())
     wait = WebDriverWait(driver, 15)
-
     try:
         driver.get("https://www.saucedemo.com")
-
         wait.until(EC.visibility_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
         driver.find_element(By.ID, "password").send_keys("secret_sauce")
         driver.find_element(By.ID, "login-button").click()
-        time.sleep(11)
 
-        
         items = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "btn_inventory")))
         for item in items:
-            driver.execute_script("arguments[0].click();", item) 
-            time.sleep(0.6)
+            driver.execute_script("arguments[0].click();", item)
 
-        
         cart_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link")))
-        driver.execute_script("arguments[0].click();", cart_button) 
-        time.sleep(3)
+        driver.execute_script("arguments[0].click();", cart_button)
 
-        
         cart_items = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "cart_item")))
-        time.sleep(3)
         assert len(cart_items) == 6
         print("SUCCESS: All 6 items in cart")
-        
-
     except Exception as e:
         driver.save_screenshot("failure_all_items.png")
-        print(driver.page_source)
         raise e
     finally:
         driver.quit()
